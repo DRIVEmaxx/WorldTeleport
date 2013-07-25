90389384cb418fcff9d595ea66163890c551e0e8
Ales Zatloukal <ajazat@seznam.cz>
2013-07-25 18:51:44 +0200

    Added onEnable and onDisable function

--- src/me/DRIVEmaxx/WorldTeleport/WorldTeleport.java
+++ src/me/DRIVEmaxx/WorldTeleport/WorldTeleport.java
@@ -1,5 +1,21 @@
 package me.DRIVEmaxx.WorldTeleport;
 
-public class WorldTeleport {
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WorldTeleport  extends JavaPlugin {
+	Logger logger = Logger.getLogger("Minecraft");
+	
+    public void onDisable(){
+        PluginDescriptionFile pdfFile = this.getDescription();
+        this.logger.info(pdfFile.getName() + " has Been Disabled!");
+    }
+ 
+    public void onEnable(){
+        PluginDescriptionFile pdfFile = this.getDescription();
+        this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " has been enabled.");
+    }
 
 }
